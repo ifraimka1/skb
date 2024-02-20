928c0620912fa4d45a200f77ab5d1bf273dbf9e7
ifraimka1 <mr.ifraim@yandex.ru>
2024-02-20 11:16:15 +0300

    Ссылки футера, добавление эффекта на ссылки в заголовках

--- src/components/Footer/FooterLink.jsx
+++ src/components/Footer/FooterLink.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function FooterLink({ link }) {
     return (
-        <Link to={ link.to } className="link">
+        <Link to={ link.to } className="link" target="_blank">
             <img src={ link.icon } alt='icon' className='icon'/>
         </Link>
     );
