95b2ee8c4e3b62c599a2728749080ffee1423094
ifraimka1 <mr.ifraim@yandex.ru>
2024-02-20 13:58:07 +0300

    Карусель + корректировка стилей под нее

---
+++ src/images/partners/index.js
@@ -0,0 +1,2 @@
+export { default as petronas } from './petronas.jpeg';
+export { default as porsche } from './porsche.svg';
--- src/pages/Main/components/Partners/Partners.jsx
+++ src/pages/Main/components/Partners/Partners.jsx
@@ -1,15 +1,44 @@
 import ScrollCarousel from 'scroll-carousel-react';
 
 import BlockHeading from '../BlockHeading';
+import PartnersLogo from './PartnersLogo';
 import './Partners.styles.scss';
+import { scb } from '../../../../images';
+import { porsche } from '../../../../images/partners';
+import { SCOPABLE_TYPES } from '@babel/types';
 
-function Partners() {
+const mock = [
+    {
+        image: porsche,
+    },
+    {
+        image: scb,
+    },
+    {
+        image: porsche,
+    },
+    {
+        image: scb,
+    },
+    {
+        image: porsche,
+    },
+    {
+        image: scb,
+    },
+];
+
+function Partners({ partners = mock }) {
     return (
         <div className="block partners">
             <BlockHeading heading="Наши партнеры" />
-            <div className="row">
-                
-            </div>
+            <ScrollCarousel 
+                autoplay
+                autoplaySpeed={6}
+                speed={1}
+            >
+                { partners.map(item => <PartnersLogo partner={ item } />) }
+            </ScrollCarousel>
         </div>
     );
 }
