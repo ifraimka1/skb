bd0498e5bdbbe523607fce3b5ae1c910ee98f46b
ifraimka1 <mr.ifraim@yandex.ru>
2024-02-20 12:22:27 +0300

    Доделал внешку формы

--- src/images/index.js
+++ src/images/index.js
@@ -1,3 +1,4 @@
 export { default as car } from './car.jpeg';
 export { default as newsMock } from './newsMock.png';
 export { default as scb } from './scb.svg';
+export { default as fileUpload } from './fileUpload.svg';
--- src/pages/Main/components/ContactUs/ContactUs.jsx
+++ src/pages/Main/components/ContactUs/ContactUs.jsx
@@ -1,5 +1,6 @@
 import { Form } from "react-router-dom";
 
+import { fileUpload } from "../../../../images";
 import './ContactUs.styles.scss';
 
 function ContactUs() {
@@ -9,11 +10,17 @@ function ContactUs() {
                 IT-специалистов готова вам помочь!</h1>
             <Form method="post" className="half contact-form">
                 <h2>Свяжитесь с нами!</h2>
-                <input type="text" name="name" />
-                <input type="email" name="email" />
-                <input type="text" name="text" />
-                <input type="file" name="file" />
-                <input type="submit" className="submit btn" />
+                <div className="inputs">
+                    <input type="text" name="name" placeholder="Имя" className="field" />
+                    <input type="email" name="email" placeholder="E-mail" className="field" />
+                    <textarea name="text" placeholder="Опишите в этом поле свой проект и, по возможности, прикрепите файл с ТЗ" className="field big" />
+                    <label className="file-input">
+                        <input type="file" name="file" />
+                        <img src={ fileUpload } alt="" className="icon" />
+                        <span>Прикрепить файл</span>
+                    </label>
+                    <input type="submit" className="submit btn" />
+                </div>
             </Form>
         </div>
     );
