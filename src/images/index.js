9d8d0daea406cb16f51ff0851a802198ed4ace3d
Ifraim Solomonov <mr.ifraim@yandex.ru>
2024-02-28 12:52:21 +0300

    анимация карточек

--- src/images/index.js
+++ src/images/index.js
@@ -3,3 +3,4 @@ export { default as newsMock } from './newsMock.png';
 export { default as scb } from './scb.svg';
 export { default as fileUpload } from './fileUpload.svg';
 export { default as arrow } from './arrow.svg';
+export { default as labMock } from './labMock.jpeg';
