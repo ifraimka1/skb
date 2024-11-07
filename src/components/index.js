aa058acdb07d526ec272cacdbd0b7a3e7f4af644
ifraimka <mr.ifraim@yandex.ru>
2024-11-07 16:38:34 +0300

    Оптимизировал список и картчоки лабораторий

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export { default as ContactForm } from './ContactForm';
 export { default as Field } from './Field';
 export { default as Footer } from './Footer';
+export { LabList } from './Labs';
 export { default as MediaBlock } from './MediaBlock';
 export { default as MediaContainer } from './MediaContainer';
 export { default as Navbar } from './Navbar';
