0f5d1dbb0acabd60aef60bca3fcc53990f824e77
ifraimka1 <mr.ifraim@yandex.ru>
2025-03-17 10:08:02 +0300

    Сделал автозаполнение на всю ширину

--- src/pages/Main/components/Partners/Partners.jsx
+++ src/pages/Main/components/Partners/Partners.jsx
@@ -23,29 +23,14 @@ const mock = [
 
 function Partners({ partners = mock }) {
     const [mediaList, setMediaList] = useState(partners);
-    // TODO: сделать отдельно init и отдельно update. Сделать статичными при <5
-    const updateMediaList = async () => {
-        // const newMediaList = await getMedia('partners');
-        // if (newMediaList.length < 2) return; // Если картинок меньше двух, нет смысла чередовать
-        
-        // const items = Math.ceil(window.innerWidth / 200); // 200px — примерная ширина одной картинки
-        // const repeatTimes = Math.ceil(items / newMediaList.length);
 
-        // for (let i = 0; i < repeatTimes; i++) {
-        //     newMediaList.push(...newMediaList);
-        // }
-
-        // console.log('Я ОБНОВЛЯЮСЬ');
-
-        // setMediaList(newMediaList);
-    };
+    const initMediaList = async () => {
+        const newMediaList = await getMedia('partners');
+        setMediaList(newMediaList);
+    }
 
     useEffect(() => {
-        updateMediaList();
-        window.addEventListener('resize', updateMediaList); // Обработчик изменения размера окна
-        return () => {
-            window.removeEventListener('resize', updateMediaList);
-        };
+        initMediaList();
     }, []);
 
     return (
@@ -57,6 +42,7 @@ function Partners({ partners = mock }) {
                     pauseOnHover={false} 
                     speed={80} 
                     className="marquee"
+                    autoFill={true}
                 >
                     {mediaList.map((item, index) => (
                         <div key={index} className="partner">
