49cd129b528bef909dc26c893bbaf618394b403d
xomimishka <mskliarova@sfedu.ru>
2025-03-25 13:17:05 +0300

    изменила футер в соответствии с дизайном фигеме

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 import FooterLink from './FooterLink';
 import FooterContacts from './FooterContacts';
@@ -9,13 +10,13 @@ import { iconIctis, iconSfedu, iconVK } from '../../assets/images/footer';
 import './Footer.styles.scss';
 
 const mockLinks = {
-    ictis: {
-        src: iconIctis,
-        to: "https://ictis.sfedu.ru",
-    },
     sfedu: {
         src: iconSfedu,
         to: "https://sfedu.ru",
+    },
+    ictis: {
+        src: iconIctis,
+        to: "https://ictis.sfedu.ru",
     },
     vk: {
         src: iconVK,
@@ -29,7 +30,7 @@ const mockContacts = {
     ),
     address: (
         <a style={{ color: 'inherit' }} href="https://yandex.ru/maps/?text=г.%20Таганрог,%20пер.%20Тургеневский,%2044" target="_blank" rel="noopener noreferrer">
-            г. Таганрог, пер. Тургеневский, 44
+            <a style={{ color: '#898989' }}>Таганрог</a><br />пер. Тургеневский, 44
         </a>
     ),
     phone: (
@@ -37,7 +38,7 @@ const mockContacts = {
     ),
 };
 
-function Footer({ links = mockLinks, contacts =  mockContacts }) {
+function Footer({ links = mockLinks, contacts = mockContacts }) {
     const [mediaList, setMediaList] = useState(links);
 
     useEffect(() => {
@@ -52,13 +53,35 @@ function Footer({ links = mockLinks, contacts =  mockContacts }) {
     return (
         <footer id="footer">
             <div className="logo-container">
-                <img className="logo" alt="logo" src={ logo } />
+                <div className="logo-container" style={{ alignItems: 'flex-start' }}>
+                    <img className="logo" alt="logo" src={logo} />
+                </div>
+                <label style={{ color: 'white', fontSize: '14px', alignItems: 'flex-end', lineHeight: 'none' }}>©СКБ «КИТ» 2015–2025</label>
             </div>
-            <FooterContacts contacts={ contacts } />
-            <div className="links">
-                { Object.keys(mediaList).map(link => <FooterLink link={ mediaList[link] } key={ mediaList[link].id } />) }
+            <div className="footer-links-container">
+                <div className="footer-links">
+                    <div className="footer-column">
+                        <Link href="#" to="/news">НОВОСТИ</Link>
+                        <Link href="#" to="/projects">ПРОЕКТЫ</Link>
+                        <Link href="#" to="/labs">ЛАБОРАТОРИИ</Link>
+                        <Link href="#" to="/aboutus">О НАС</Link>
+                    </div>
+                    <div className="footer-column">
+                        <Link href="#" >НАША КОМАНДА</Link>
+                        <Link href="#" to="/contact">КОНТАКТЫ</Link>
+                        <Link href="#" to="https://ictis.sfedu.ru">ИКТИБ</Link>
+                    </div>
+                </div>
+                <Link style={{ color: 'white', fontSize: '14px', alignItems: 'flex-end', lineHeight: 'none' }} href="#" to="https://www.study.sfedu.ru/privacypolicy?ysclid=m8anp507sz44008873">Политика конфиденциальности</Link>
             </div>
-        </footer>
+
+            <div className="footer-content">
+                <FooterContacts contacts={contacts} />
+                <div className="links">
+                    {Object.keys(mediaList).map(link => <FooterLink link={mediaList[link]} key={mediaList[link].id} />)}
+                </div>
+            </div>
+        </footer >
     );
 }
 
--- src/components/Footer/FooterContacts.jsx
+++ src/components/Footer/FooterContacts.jsx
@@ -1,9 +1,9 @@
 function FooterContacts({ contacts }) {
     return (
         <div className="contacts">
-            <p>{ contacts.email }</p>
-            <p>{ contacts.address }</p>
-            <p>{ contacts.phone }</p>
+            <p style={{ fontSize: '24px', fontWeight: '500', textDecoration: 'underline', textDecorationThickness: '1px', textUnderlineOffset: '2px' }}>{ contacts.email }</p>
+            <p style={{ fontSize: '24px' }}>{ contacts.phone }</p>
+            <p style={{ fontSize: '16px', lineHeight: '25px', fontWeight: '300' }}>{ contacts.address }</p>
         </div>
     );
 }
