8d0ce2827a7881cd25efa23731f64d1fcfb8e341
xomimishka <mskliarova@sfedu.ru>
2025-04-17 15:58:59 +0300

    убрала картинки чисел

--- src/shared/assets/images/numbers/index.js
+++ src/shared/assets/images/numbers/index.js
@@ -1,4 +1,1 @@
-export { default as ten } from './ten.svg';
-export { default as twenty } from './twenty.svg';
-export { default as thirty } from './thirty.svg';
 export { default as Illustration } from './Illustration.png';
