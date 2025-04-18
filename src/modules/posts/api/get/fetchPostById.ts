eb6d72f5db3236f2fafb0bb49e20bc3dad08fe9b
ifraimka1 <mr.ifraim@yandex.ru>
2025-04-18 13:53:42 +0300

    Убрал категорию в получении одного поста по ID

--- src/modules/posts/api/get/fetchPostById.ts
+++ src/modules/posts/api/get/fetchPostById.ts
@@ -4,24 +4,12 @@ import { parseContent } from "@/shared/api/parseContent";
 import { App, WPv2 } from "@/shared/types/app";
 
 export const fetchPostById = async (id: number): Promise<App.WpPostPage> => {
-  const [post, categories] = await Promise.all([
-    wp.posts().id(id).get() as Promise<WPv2.Post>,
-    wp.categories().get() as Promise<WPv2.Category[]>
-  ]);
-
-  const categoryMap = new Map<number, string>(
-    categories.map((c) => [c.id, c.name])
-  );
-
-  const postCategories = post.categories.map(
-    (id) => categoryMap.get(id) || ""
-  );
+  const post = await wp.posts().id(id).get() as WPv2.Post;
 
   const result: App.WpPostPage = {
     id: post.id,
     title: post.title.rendered,
     content: parseContent(post.content.rendered),
-    categories: postCategories,
     tag: post.tags[0],
   };
 
