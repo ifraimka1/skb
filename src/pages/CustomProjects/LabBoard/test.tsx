35909dce1ffc637ca3655f9df89c044217efa6de
xomimishka <mskliarova@sfedu.ru>
2025-05-23 14:03:48 +0300

    связывание записей при помощи titel

---
+++ src/pages/CustomProjects/LabBoard/test.tsx
@@ -0,0 +1,7 @@
+import { HashLink } from "react-router-hash-link";
+
+function Test() {
+  return <>ddddddddddddd</>;
+}
+export default Test;
+
--- src/widgets/WpPost/WpPost.tsx
+++ src/widgets/WpPost/WpPost.tsx
@@ -7,7 +7,7 @@ import { App } from "@/shared/types/app";
 
 import "./Projects.scss";
 import Slider from "../Slider/Slider";
-import LabBoard from "@/pages/CustomProjects/LabBoard/LabBoard";
+import { getCustomProject } from "./customProjects";
 
 interface WpPostProps {
   post: App.WpPostPage;
@@ -18,32 +18,35 @@ export default function WpPost({
   post,
   children,
 }: WpPostProps) {
-  useResizeObserver({ parentSelector: '.wp-block-pullquote', childSelector: 'blockquote' });
+  useResizeObserver({ parentSelector: ".wp-block-pullquote", childSelector: "blockquote" });
 
-  const isLabBoardPost = post.title === "LabBoard 1.1";
+  const categories = post.categories ?? [];
+  const title = post.title;
+  const customComponent = getCustomProject(title);
 
   return (
-    <div className={post.categories?.includes('projects') ? "project" : ""}>
-      {!isLabBoardPost && (
-        <PageHeader className="header">
-          <h1>{post.title}</h1>
-        </PageHeader>
+    <div className={categories.includes("projects") ? "project" : ""}>
+      {customComponent ? (
+        customComponent
+      ) : (
+        <>
+          <PageHeader className="header">
+            <h1>{post.title}</h1>
+          </PageHeader>
+          <PageContent className="content">
+            {Array.isArray(post.content) &&
+              post.content.map((el, index) => {
+                if (el.type === "mediablock") {
+                  return <MediaBlock key={index} images={el.value} />;
+                } else if (el.type === "slider") {
+                  return <Slider key={index} images={el.value} customPerSlide={4} />;
+                }
+                return <div key={index}>{el.element}</div>;
+              })}
+            {children}
+          </PageContent>
+        </>
       )}
-
-      <PageContent className="content">
-        {Array.isArray(post.content) &&
-          post.content.map((el, index) => {
-            if (el.type === "mediablock") {
-              return <MediaBlock key={index} images={el.value} />;
-            } else if (el.type === "slider") {
-              return <Slider images={el.value} customPerSlide={4} />
-            }
-            return <div key={index}>{el.element}</div>;
-          })}
-        {children}
-      </PageContent>
-
-      {isLabBoardPost && <LabBoard />}
     </div>
   );
 }
