d3a23dc716c6ffbc89e499b5046d25a9707db3b0
xomimishka <mskliarova@sfedu.ru>
2025-05-30 17:54:40 +0300

    добавлено в о нас блок трудолюбие и креативность, возможность менять фон хедера через worpress, название фона должно быть art

--- src/pages/MainPage/ui/MainPage/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage/MainPage.tsx
@@ -1,6 +1,6 @@
+import { useContext, useEffect, useState } from "react";
 import styles from "./MainPage.module.scss";
 import { HashLink } from "react-router-hash-link";
-import { useContext, useEffect } from "react";
 import { RootContext } from "@/app/Layout/Root";
 import Partners from "@/widgets/Partners";
 import Gallery from "@/pages/MainPage/ui/components/Gallery";
@@ -15,8 +15,22 @@ import Numbers from "@/widgets/Numbers";
 
 const MainPage = () => {
   const { setRef } = useContext(RootContext);
+  const [headerBgUrl, setHeaderBgUrl] = useState("");
 
   useEffect(() => {
+    // Запрос к WP REST API для поиска медиа по ключу "art"
+    fetch("https://test.skbkit.ru/wp-json/wp/v2/media?search=art")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.length > 0) {
+          // Берём первый объект и его URL картинки
+          setHeaderBgUrl(data[0].source_url);
+        }
+      })
+      .catch((err) => {
+        console.error("Ошибка загрузки фона с WP:", err);
+      });
+
     const navbar = document.getElementById("navbar");
     if (navbar) {
       navbar.classList.add("transparent");
@@ -28,9 +42,17 @@ const MainPage = () => {
       }
     };
   }, []);
+
   return (
     <div>
-      <div id={styles.mainpageheader} ref={(element) => setRef(element)}>
+      <div
+        id={styles.mainpageheader}
+        ref={(element) => setRef(element)}
+        style={{
+          backgroundImage:
+            `url(${headerBgUrl})`,
+        }}
+      >
         <section className={styles.container}>
           <h1>Думай иначе, будь креативным!</h1>
           <h2>
--- src/widgets/PhotoGallery/PhotoGallery.tsx
+++ src/widgets/PhotoGallery/PhotoGallery.tsx
@@ -3,6 +3,8 @@ import { useMedia } from "@/modules/media/hooks/useMedia";
 import { SkeletonGallery } from "@/shared/Components/SkeletonGallery/SkeletonGallery";
 
 import "./PhotoGallery.style.scss"
+import HWaC1 from "@/shared/assets/images/hard_work_and_creativity/HWaC1.jpg";
+import HWaC2 from "@/shared/assets/images/hard_work_and_creativity/HWaC2.jpg";
 
 interface PhotoGalleryProps {
   images?: string[];
@@ -31,6 +33,26 @@ const PhotoGallery = ({ images }: PhotoGalleryProps) => {
 
   return (
     <div className="mainContainer">
+      <h1 className="title">Трудолюбие и креативность</h1>
+      <div className="creativity-section">
+        <div className="creativity-text-top">
+          <p>
+            Проверено опытом: для решения задач, которые перед нами стоят,
+            важны максимальная вовлеченность в инженерное творчество и желание изменить мир к лучшему, хотя бы вокруг себя.
+          </p>
+        </div>
+        <div className="creativity-images">
+          <img src={HWaC1} />
+          <img src={HWaC2} />
+        </div>
+        <div className="creativity-text-bottom">
+          <p>
+            Создание чего‑то нового — всегда творческий процесс. Для нас очень важно умение решать возникающие трудности
+            и находить выход из нестандартных ситуаций. Мы всегда учимся, развиваемся и совершенствуемся.
+          </p>
+        </div>
+      </div>
+
       <h1 className="title">Фотогалерея</h1>
       <div className="images">
         {filteredMedia.slice(0, visibleCount).map((media) => (
