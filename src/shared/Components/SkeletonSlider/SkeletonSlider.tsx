9fc9755a745e7a810fdc62904afe0e27fccca9a1
xomimishka <mskliarova@sfedu.ru>
2025-07-11 11:36:08 +0300

    в моб версии 1 карточка шиммера

--- src/shared/Components/SkeletonSlider/SkeletonSlider.tsx
+++ src/shared/Components/SkeletonSlider/SkeletonSlider.tsx
@@ -1,10 +1,12 @@
 import styles from "./SkeletonSlider.module.scss";
 
 export const SkeletonSlider = () => {
+  const skeletonCount = window.innerWidth > 768 ? 2 : 1;
+
   return (
     <div className={styles.sliderSkeleton}>
       <div className={styles.slideContainer}>
-        {[...Array(2)].map((_, index) => (
+        {[...Array(skeletonCount)].map((_, index) => (
           <div key={index} className={styles.slide}>
             <div className={styles.image}></div>
           </div>
